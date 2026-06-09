1c3223a3948d5fc561b2d8fcc3a7960d25973811
Hotor Benjamin <hotorben001@gmail.com>
2026-06-09 22:00:24 +0000

    npm-to-pnpm6

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,7 +3,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient  {
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
